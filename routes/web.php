740a79f15a03764ec61a7cd9dbaf3b43f484d369
cosmosgc <cosmoskitsune@hotmail.com>
2024-09-18 17:05:40 -0300

    api update

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,13 @@ Route::get('/', [ComicController::class, 'index'])->name('comics.index');
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
-Route::get('/comics/{id}', [ComicController::class, 'show'])->name('comics.show');
+
+// Route to access a comic by ID
+Route::get('/comics/id/{id}', [ComicController::class, 'showById'])->name('comics.showById');
+
+// Route to access a comic by slug
+Route::get('/comics/{slug}', [ComicController::class, 'showBySlug'])->name('comics.showBySlug');
+
 
 Route::get('comics/upload', [ComicController::class, 'create'])->name('comics.upload');
 Route::post('comics/store', [ComicController::class, 'store'])->name('comics.store');
